f7c97a09e97f652224885365634b202442542033
qiushui <qiushui@zgpgc.com>
2020-02-28 23:09:39 +0800

    添加mongodb事务支持

--- resource-manager-mongodb/src/main/java/com/github/developframework/resource/spring/mongo/DOC.java
+++ resource-manager-mongodb/src/main/java/com/github/developframework/resource/spring/mongo/DOC.java
@@ -1,6 +1,7 @@
 package com.github.developframework.resource.spring.mongo;
 
 import com.github.developframework.resource.Entity;
+import org.bson.types.ObjectId;
 
 import java.io.Serializable;
 
@@ -8,4 +9,17 @@ import java.io.Serializable;
  * @author qiushui on 2019-08-21.
  */
 public interface DOC<ID extends Serializable> extends Entity<ID> {
+
+    default ObjectId getObjectId() {
+        final ID id = getId();
+        if (id == null) {
+            return null;
+        } else if (id instanceof String) {
+            return new ObjectId((String) id);
+        } else if (id instanceof ObjectId) {
+            return (ObjectId) id;
+        } else {
+            throw new IllegalArgumentException("Can not convert to ObjectId");
+        }
+    }
 }
--- resource-manager-mongodb/src/main/java/com/github/developframework/resource/spring/mongo/ResourceMongoAutoConfiguration.java
+++ resource-manager-mongodb/src/main/java/com/github/developframework/resource/spring/mongo/ResourceMongoAutoConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.List;
 
 /**
  * @author qiushui on 2019-08-24.
  */
+@EnableTransactionManagement
 @Configuration
 public class ResourceMongoAutoConfiguration {
 
