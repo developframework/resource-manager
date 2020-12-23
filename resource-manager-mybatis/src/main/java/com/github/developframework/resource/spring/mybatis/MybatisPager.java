36f27f592babd38e770697588d3688ee294db48e
qiushui <qiushui@zgpgc.com>
2020-12-23 09:25:31 +0800

    pager

---
+++ resource-manager-mybatis/src/main/java/com/github/developframework/resource/spring/mybatis/MybatisPager.java
@@ -0,0 +1,26 @@
+package com.github.developframework.resource.spring.mybatis;
+
+import develop.toolkit.base.struct.Pager;
+
+/**
+ * @author qiushui on 2020-12-23.
+ */
+public class MybatisPager extends Pager {
+
+    public MybatisPager() {
+        super();
+    }
+
+    public MybatisPager(int index, int size) {
+        super(index, size);
+    }
+
+    /**
+     * 生成 LIMIT 语句
+     *
+     * @return LIMIT 语句
+     */
+    public String limitSQL() {
+        return String.format("LIMIT %d, %d", (page - 1) * size, size);
+    }
+}
