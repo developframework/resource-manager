9f97532f85bf50169a8bde37be749c933189ba13
qiushui <qiushui@zgpgc.com>
2021-08-10 16:01:15 +0800

    开发

--- resource-manager-mybatis/src/main/java/com/github/developframework/resource/spring/mybatis/ResourceMybatisAutoConfiguration.java
+++ resource-manager-mybatis/src/main/java/com/github/developframework/resource/spring/mybatis/ResourceMybatisAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.github.developframework.resource.spring.mybatis;
 
+import develop.toolkit.base.components.SnowflakeIdWorker;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,13 @@ public class ResourceMybatisAutoConfiguration {
     @Bean
     public AutoCreateTableListener autoCreateRableListener() {
         return new AutoCreateTableListener();
+    }
+
+    @Bean
+    public SnowflakeIdWorker snowflakeIdWorker(
+            @Value("${snowflake.workerId:1}") long workerId,
+            @Value("${snowflake.datacenterId:1}") long datacenterId
+    ) {
+        return new SnowflakeIdWorker(workerId, datacenterId);
     }
 }
